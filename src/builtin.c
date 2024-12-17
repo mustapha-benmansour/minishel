9309b6383a4369ae5be5a44be9e260c9eaff4c1e
Mustapha Benmansour <mustapha.benmansour.ma@gmail.com>
2024-12-17 13:26:17 +0100

    fix

--- src/builtin.c
+++ src/builtin.c
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *functions[] = {"cd", "exit", "export", "unset",
+const char *functions[] = {"cd", "exit", "export",
 						   NULL}; // Tableau des commandes internes
 
 int is_builtin(const char *cmd) {
